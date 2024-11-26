4f3ec3dcf9970259107a80fcd193fb326d2c3eb0
Jeetu Gupta <jeetu270602@gmail.com>
2024-11-26 13:59:17 +0530

    create createParticipant api

---
+++ controller/participants.js
@@ -0,0 +1,72 @@
+const _ = require("lodash");
+const Participants = require("../model/participants");
+
+const createParticipants = async (req, res) => {
+  try {
+    console.log(req.body);
+    const {
+      name,
+      email,
+      phone,
+      division,
+      standard,
+      class_roll_no,
+      hackerrank_username,
+    } = req.body;
+
+    if (
+      _.isNil(name) ||
+      _.isNil(email) ||
+      _.isNil(phone) ||
+      _.isNil(division) ||
+      _.isNil(standard) ||
+      _.isNil(class_roll_no) ||
+      _.isNil(hackerrank_username)
+    ) {
+      return res.send({
+        status: 400,
+        success: false,
+        message: "Mandatory fields are missing",
+        data: {},
+      });
+    }
+
+    const existingParticipant = await Participants.findOne({ email });
+
+    if (!_.isNil(existingParticipant)) {
+      return res.send({
+        status: 400,
+        success: false,
+        message: "Participant already exists",
+        data: {},
+      });
+    }
+
+    const participants = await Participants.create({
+      email,
+      name,
+      class_roll_no,
+      class: standard,
+      division,
+      phone,
+      hackerrank_username,
+    });
+
+    return res.send({
+      status: 200,
+      success: true,
+      message: "Participants created successfully",
+      data: participants,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: 500,
+      success: false,
+      message: "Error creating participants",
+      data: {},
+    });
+  }
+};
+
+module.exports = { createParticipants };
