6248d20780a7840621d0a4fb00d3002ba1d95915
Jeetu Gupta <jeetu270602@gmail.com>
2024-11-27 15:50:29 +0530

    add test route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 const { createParticipants } = require("../controller/participants");
 
+router.get("/", (req, res) => {
+  return res.send("This is the CodeClash API");
+});
 router.get("/v1/test", (req, res) => {
-  res.send("This is a test route");
+  res.send("This is a test route for v1 API");
 });
 
 router.post("/v1/participants", createParticipants);
